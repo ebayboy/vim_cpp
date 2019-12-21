9639079a9e3e7bc44131fba64d7b9355b37f375b
ebayboy <ebayboy@163.com>
2019-12-21 15:58:40 +0800

    fw

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# ***************************************************************************
+# * 
+# * @file:build.sh 
+# * @author:ebayboy@163.com 
+# * @date:2019-12-21 15:56 
+# * @version 1.0  
+# * @description: Shell script 
+# * @Copyright (c)  all right reserved 
+#* 
+#**************************************************************************/ 
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+exit 0
+
